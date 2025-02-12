cdea0ad44ab244ae91d26a2c3dd60850f349e89b
Isaque Barbosa <isaquebatista400@gmail.com>
2025-02-12 18:28:01 -0300

    :sparkles: feat: Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header.tsx';
+import Home from './pages/Home/Home.tsx';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Home />
+    </>
   );
 }
 
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import "./styles.css"
+import homePlant from "../../assets/image/home-plant.png"
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <section className="text-section">
+        <div className="text-section-title">
+          <h1>Hi I'm <span>Isaque</span>, <br />Web Developer</h1>
+        </div>
+        <div className="text-section-subtitle">Currently I am a student and developer, my focus is web development with React TS</div>
+      </section>
+      <img src={homePlant} alt="home plant" className="home-image"/>
+    </div>
+  )
+}
